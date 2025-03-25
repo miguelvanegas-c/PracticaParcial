fb6f655f8b8c4c441e06b970e2889c61dd15b2ba
Miguel <miguel.vanegas-c@mail.escuelaing.edu.co>
2025-03-25 15:13:17 -0500

    first commit

---
+++ CRUD/src/test/java/practica/parcial/CRUD/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package practica.parcial.CRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
